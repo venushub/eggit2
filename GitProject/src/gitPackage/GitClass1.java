1d80758826f7bf5eb678383a6a73b6b3cea8fada
venushub <venu.guntamukkala93@gmail.com>
2018-02-06 15:04:03 +0530

    added variable c

--- GitProject/src/gitPackage/GitClass1.java
+++ GitProject/src/gitPackage/GitClass1.java
@@ -6,8 +6,10 @@ public class GitClass1 {
 		// TODO Auto-generated method stub
 		int a = 10;
 		int b = 20;
+		int c = 30;
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
 	}
 
 }
